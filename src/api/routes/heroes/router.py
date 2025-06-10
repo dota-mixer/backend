8212f2017b55f856c8bf138b5b87fa0ea7573d2b
TemaDrakoshef <artem125149@gmail.com>
2025-06-10 23:56:34 +0300

    Add the ability to get a hero by NPC name

--- src/api/routes/heroes/router.py
+++ src/api/routes/heroes/router.py
@@ -1,15 +1,26 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
-from src.api.routes.heroes.schemas import HeroesOut
-from src.parsers.protracker.api import get_heroes_list
+from src.api.routes.heroes.schemas import HeroOut, HeroesOut
+from src.parsers.protracker.api import get_heroes_list, get_hero_by_name
 
 heroes_router = APIRouter(prefix="/heroes", tags=["heroes"])
 
 
 @heroes_router.get("/")
-async def get_heroes() -> HeroesOut:
+async def read_heroes() -> HeroesOut:
     """
     Get the list of heroes.
     """
     heroes = await get_heroes_list()
     return HeroesOut(data=heroes, count=len(heroes))
+
+
+@heroes_router.get("/{name}")
+async def read_hero_by_name(name: str) -> HeroOut:
+    """
+    Get the hero by NPC name.
+    """
+    hero = await get_hero_by_name(name=name)
+    if hero is None:
+        raise HTTPException(status_code=404, detail="Hero not found")
+    return hero
--- src/parsers/protracker/api.py
+++ src/parsers/protracker/api.py
@@ -21,3 +21,17 @@ async def get_heroes_list() -> dict:
             response=response,
         )
     return convert_json_to_heroes(response.json())
+
+
+async def get_hero_by_name(name: str) -> dict:
+    """
+    Get a hero by its NPC name from Dota 2 Pro Tracker API.
+
+    Args:
+        name: The NPC name of the hero.
+    """
+    heroes = await get_heroes_list()
+    for hero in heroes:
+        if hero.npc == name:
+            return hero
+    return None
