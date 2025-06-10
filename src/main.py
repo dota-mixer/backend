177233460599b8183fc7bc61955791d0ed9e2548
TemaDrakoshef <artem125149@gmail.com>
2025-06-10 23:55:15 +0300

    Add CORS middleware

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.routing import APIRoute
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.api.main import api_router
 
@@ -14,6 +15,14 @@ app = FastAPI(
     title="Dota Mixer API",
     openapi_url=f"{API_V1_STR}/openapi.json",
     generate_unique_id_function=custom_generate_unique_id,
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="*",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(api_router, prefix=API_V1_STR)
